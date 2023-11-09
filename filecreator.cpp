1011c2df4050c978907f52b1b511a1c4fdf89686
wheatgreaser <108815657+wheatgreaser@users.noreply.github.com>
2023-11-09 20:45:00 +0530

    Add files via upload

---
+++ filecreator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  fstream file;
+  file.open("coolman.txt", ios::out);
+  cout << "yup created";
+  file.close();
+  return 0;
+  
+  
+}
